e8d892d1676936ef9eb5aa5cd1fa91c805275848
Lagerst <pzw9908@outlook.com>
2020-03-04 16:46:40 +0800

    20200304

--- LeetCode_Rust/src/main.rs
+++ LeetCode_Rust/src/main.rs
@@ -1,3 +1,34 @@
+struct Scanner {
+    buffer : std::collections::VecDeque<String>
+ }
+
+ impl Scanner {
+
+    fn new() -> Scanner {
+       Scanner {
+          buffer: std::collections::VecDeque::new()
+       }
+    }
+
+    fn next<T : std::str::FromStr >(&mut self) -> T {
+       if self.buffer.len() == 0 {
+          let mut input = String::new();
+          std::io::stdin().read_line(&mut input).ok();
+          for word in input.split_whitespace() {
+             self.buffer.push_back(word.to_string())
+          }
+       }
+       let front = self.buffer.pop_front().unwrap();
+       front.parse::<T>().ok().unwrap()
+    }
+
+}
+
 fn main() {
-    
+    let mut scanner = Scanner::new();
+    let n : usize = scanner.next();
+    for _ in 0..n {
+        let num : i32 = scanner.next();
+        println!("{}", num);
+    }
 }
