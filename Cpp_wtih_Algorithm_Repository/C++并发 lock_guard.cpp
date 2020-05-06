353d4d0c3db79c43e6e4969a3584168475b308df
pengziwei.1999 <pengziwei.1999@bytedance.com>
2020-05-06 10:46:23 +0800

    2020年 5月 6日 星期三 10时46分23秒 CST

---
+++ Cpp_wtih_Algorithm_Repository/C++并发 lock_guard.cpp
@@ -0,0 +1,27 @@
+#include <list>
+#include <mutex>
+#include <algorithm>
+#include <iostream>
+ 
+std::list<int> some_list;
+std::mutex value_mutex;
+ 
+void add_to_list(int new_value)
+{
+    std::lock_guard<std::mutex> mutex(value_mutex);
+    some_list.push_back(new_value);
+}
+ 
+bool list_contains(int value_to_find)
+{
+    std::lock_guard<std::mutex> mutex(value_mutex);
+    return std::find(some_list.begin(), some_list.end(), value_to_find) != some_list.end();
+}
+ 
+ 
+int main()
+{
+    add_to_list(33);
+    std::cout << "contains(1)=" << list_contains(1) << ",contains(33)=" << list_contains(33) << std::endl;
+    return 0;
+}
